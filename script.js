28c320b455a3fd6ef4a9dea4c479bac083b4a2e5
mstsurnalyakter <surnalyms@gmail.com>
2024-02-11 18:02:13 +0600

    UI Design added

---
+++ script.js
@@ -0,0 +1,17 @@
+const subhanAllahDisplay = document.getElementById("subhanAllahDisplay");
+const subhanAllahIncrimentBtn = document.getElementById("subhanAllahIncrimentBtn");
+const subhanAllahDecrimentBtn = document.getElementById("subhanAllahDecrimentBtn");
+
+const alhamdulillahDisplay = document.getElementById("alhamdulillahDisplay");
+const alhamdulillahIncrimentBtn = document.getElementById("alhamdulillahIncrimentBtn");
+const alhamdulillahDecrimentBtn = document.getElementById("alhamdulillahDecrimentBtn");
+
+
+const allahAkberDisplay = document.getElementById("allahAkberDisplay");
+const allahAkberIncrimentBtn = document.getElementById("allahAkberIncrimentBtn");
+const allahAkberDecrimentBtn = document.getElementById("allahAkberDecrimentBtn");
+
+
+const resetBtn = document.getElementById("resetBtn");
+
+
